2fc85b99ba2a42cf0000305e7f7b4dc4d68736ed
Manozz <flutterprjsbym33@gmail.com>
2025-12-18 21:04:01 +0545

    Categories RemoteDataSoure

--- lib/features/catogories/data/cat_data_source/CatRepoLocalDataSource.dart
+++ lib/features/catogories/data/cat_data_source/CatRepoLocalDataSource.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
+import 'package:shoe/core/utils/CustomExpn.dart';
+import 'package:shoe/features/catogories/data/cat_model/cat_items_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CatRepoLocalDataSource{
   final SupabaseClient supabaseClient;
   CatRepoLocalDataSource({required this.supabaseClient});
+
+
+ Future<List<CategoryModel>> getCatoFromRemoteDataSource()async
+ {
+   try{
+     final rawCato =  await supabaseClient.from('shoe_cat').select();
+     final catoList = rawCato.map((e)=>CategoryModel.fromJson(e)).toList();
+     return catoList;
+
+   }on SocketException {
+     throw NoInternetException();
+   } catch (e)
+   {
+     throw ServerErrorException();
+   }
+   
+ }
 
 
 
---
+++ lib/features/catogories/data/cat_model/cat_items_model.dart
@@ -0,0 +1,16 @@
+import 'package:shoe/features/catogories/domain/cato_entity/categories_entity.dart';
+
+class CategoryModel extends CategoriesEntity {
+  CategoryModel({required super.id, required super.cat_image, required super.cat_title});
+
+  factory CategoryModel.fromJson(Map<String,dynamic> map)
+  {
+    return CategoryModel(
+        id: map['id'] ?? '',
+        cat_image: map['cat_img'] ?? '',
+        cat_title: map['cat_title'] ?? '');
+  }
+
+
+
+}
