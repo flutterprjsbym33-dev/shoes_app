0420be991a6ad09b05fac1a2a9bb3f29b42ae2ec
Manozz <flutterprjsbym33@gmail.com>
2025-12-20 20:52:15 +0545

    use case added

---
+++ lib/features/products/domain/product_use_cases/get_product.dart
@@ -0,0 +1,24 @@
+import 'package:dart_either/src/dart_either.dart';
+
+import 'package:shoe/features/banners/domain/failure.dart';
+
+import '../product_entity/shoe.dart';
+import '../product_repo/shoe_repo.dart';
+
+class GetShoes {
+  final ShoeRepository repository;
+
+  GetShoes(this.repository);
+
+  Either<Failure, List<Shoe>> call({
+    String? brand,
+    required int page,
+    required int limit,
+  }) {
+    return repository.getShoes(
+      brand: brand,
+      page: page,
+      limit: limit,
+    );
+  }
+}
