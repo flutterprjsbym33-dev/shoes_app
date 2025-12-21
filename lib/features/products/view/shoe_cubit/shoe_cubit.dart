5c0e16dbfe7523aba547d3fa5ed70b060d7a235b
Manozz <flutterprjsbym33@gmail.com>
2025-12-21 21:11:04 +0545

    cubit created for fetching

---
+++ lib/features/products/view/shoe_cubit/shoe_cubit.dart
@@ -0,0 +1,75 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shoe/features/products/domain/product_use_cases/get_product.dart';
+import 'package:shoe/features/products/view/shoe_cubit/shoe_state.dart';
+
+class FetchShoeCubit extends Cubit<ShoeState> {
+  int limit = 4;
+  GetShoes getShoes;
+  FetchShoeCubit({
+    required this.getShoes
+}):super(ShoeState.initial());
+
+  Future<void> getShoesForHome()async
+  {
+    if(state.isLoading && state.isReachedMax) return;
+
+
+    emit(ShoeState(shoes: state.shoes,
+        isReachedMax: state.isReachedMax,
+        page: state.page,
+        isLoading: true));
+
+    final newShoes = await getShoes(
+      brand: '',
+      page: state.page,
+      limit: limit,
+    );
+
+    newShoes.fold(ifLeft: (failure)=>emit(
+        ShoeState(shoes: [],
+        isReachedMax: false,
+        page:state.page,
+        isLoading: false,
+        errMsg: failure.message)),
+        ifRight: (success)=>emit(
+          ShoeState(
+              shoes: [...state.shoes,...success],
+              isReachedMax: success.length<limit,
+              page: state.page+1,
+              isLoading: false)
+        ));
+
+  }
+
+  Future<void> getShoesFormBrand(String brand)async
+  {
+    if(state.isLoading && state.isReachedMax) return;
+
+
+    emit(ShoeState.initial());
+
+    final newShoes = await getShoes(
+      brand: brand,
+      page: state.page,
+      limit: limit,
+    );
+
+    newShoes.fold(ifLeft: (failure)=>emit(
+        ShoeState(shoes: [],
+            isReachedMax: false,
+            page:state.page,
+            isLoading: false,
+            errMsg: failure.message)),
+        ifRight: (success)=>emit(
+            ShoeState(
+                shoes: success,
+                isReachedMax: success.length<limit,
+                page: state.page+1,
+                isLoading: false)
+        ));
+
+  }
+
+
+
+}
--- lib/features/products/view/shoe_cubit/shoe_state.dart
+++ lib/features/products/view/shoe_cubit/shoe_state.dart
@@ -5,15 +5,17 @@ class ShoeState{
   bool isLoading;
   bool isReachedMax;
   int page;
+  String? errMsg;
 
   ShoeState({
     required this.shoes,
     required this.isReachedMax,
     required this.page,
     required this.isLoading,
+     this.errMsg
   });
 
-  factory ShoeState.initia()
+  factory ShoeState.initial()
   {
     return ShoeState(
         shoes: [],
