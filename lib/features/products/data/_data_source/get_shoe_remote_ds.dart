0c2ea29c279d1aaad1bbb1a8f3ad9775cb3cc285
Manozz <flutterprjsbym33@gmail.com>
2025-12-21 20:33:49 +0545

    method called

--- lib/features/products/data/_data_source/get_shoe_remote_ds.dart
+++ lib/features/products/data/_data_source/get_shoe_remote_ds.dart
@@ -1,5 +1,46 @@
+import 'dart:io';
+
+import 'package:shoe/core/utils/CustomExpn.dart';
 import 'package:shoe/features/products/data/model/shoe_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class GetShoeRemoteDataSource{
-  Future<List<ShoeModel>> getShoeFromSupabase();
+  Future<List<ShoeModel>> getShoeFromSupabase({
+    String? brand,
+    required int page,
+    required int limit
+  });
+}
+
+class FetchSoesRemoteDataSource extends GetShoeRemoteDataSource{
+  SupabaseClient supabaseClient;
+  FetchSoesRemoteDataSource({required this.supabaseClient});
+
+  @override
+  Future<List<ShoeModel>> getShoeFromSupabase({String? brand, required int page, required int limit})async
+  {
+    try{
+      final from = (page-1)*limit;
+      final to = from+limit-1;
+      var querry = supabaseClient.from('shoe').select().eq('is_active', true);
+
+      if(brand!=null)
+      {
+        querry = querry.eq('brand', brand);
+      }
+
+      final data = await querry.order('created_at',ascending: false).range(from, to) as List;
+
+      return data.map((e)=>ShoeModel.fromJson(e)).toList();
+    } on SocketException {
+      throw NoInternetException();
+    } catch (e)
+    {
+      throw ServerErrorException();
+    }
+
+  }
+
+
+
 }
