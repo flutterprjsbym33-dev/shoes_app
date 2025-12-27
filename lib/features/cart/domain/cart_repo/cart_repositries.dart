00720d9735031f3f0b470690a7d8590b5bb679cc
Manozz <flutterprjsbym33@gmail.com>
2025-12-27 21:49:45 +0545

    cart repo update

--- lib/features/cart/domain/cart_repo/cart_repositries.dart
+++ lib/features/cart/domain/cart_repo/cart_repositries.dart
@@ -3,10 +3,10 @@ import '../cart_entity/cart_entity.dart';
 
 abstract class CartRepository {
 //add to cart
-  Future<Cart> addToCart(Shoe shoe);
+  Future<Cart> addToCart(Cart cart);
 
   // remove from cart
-Future<void> removeFromCart(String id);
+Future<void> removeFromCart(String shoeId);
 
 //update quantity
 
